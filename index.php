de374db6da72d2dfc76cd258f3dd34abac46a0e4
Dylan Reuter <djreuter45@gmail.com>
2017-07-12 14:00:03 -0600

    added table, ul, and ol

--- index.php
+++ index.php
@@ -6,5 +6,37 @@
 	</head>
 	<body>
 		<h1>This is my web page</h1>
+		<table>
+			<tr>
+				<th>Cats</th>
+				<th>Dogs</th>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Maine Coon</td>
+				<td>Rottweiler</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shepherd</td>
+			</tr>
+		</table>
+
+		<h2>A few of My Favorite Things</h2>
+		<ul>
+			<li>Soccer</li>
+			<li>Music</li>
+			<li>Snowboarding</li>
+		</ul>
+
+		<h2>Things to Do Today</h2>
+		<ol>
+			<li>Study</li>
+			<li>Watch Gold Cup</li>
+			<li>Study some more</li>
+		</ol>
 	</body>
 </html>
