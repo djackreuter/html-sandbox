cc295e6d5b712e1fdedd4f8c5baad2859146c2cb
Dylan Reuter <djreuter45@gmail.com>
2017-07-31 10:21:11 -0600

    added some content to manipulate with grid system

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!-- Font Awesome -->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
@@ -24,15 +26,113 @@
 		<![endif]-->
 
 		<!-- JQuery v3.0 -->
-		<script> src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"</script>
+		<script> src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"</script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>This is my web page</h1>
+		<!-- begin grid layout -->
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<h1>Hello Bootstrap!</h1>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h2>Column 1</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit, ante sed lacinia consequat,
+						eros ligula tristique dui, et aliquam sem arcu vel justo. Etiam eu turpis quis odio consectetur
+						posuere. Nullam congue eros sit amet magna feugiat, eu placerat mi pellentesque. Proin mollis aliquam
+						lacus, a vestibulum nunc dignissim a. Ut faucibus eu leo a fermentum. Etiam sed feugiat ligula, in
+						hendrerit ipsum. Quisque sodales tellus sed sem porta, mattis sodales orci pretium. Nullam elementum
+						turpis nec nisi ullamcorper mollis et nec velit. Curabitur consectetur ante ac dolor finibus, sed
+						egestas velit dignissim.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h2>Column 2</h2>
+					<p>Aliquam nec ipsum mauris. Phasellus ultricies ante arcu, ut mollis erat bibendum eget. Sed id sodales
+						urna, convallis sagittis libero. Vestibulum ornare purus eget nulla porttitor fringilla. Morbi
+						scelerisque at nunc vitae ultrices. Maecenas feugiat velit arcu, sit amet congue mi tempor nec.
+						Vivamus leo dui, aliquam ut molestie nec, semper id dolor. In tincidunt, orci at laoreet pharetra,
+						metus quam ultrices nunc, eget tempor metus augue vel tortor. Vivamus orci odio, fermentum in quam
+						vitae, venenatis blandit urna. Vestibulum gravida dignissim quam, vel cursus ligula maximus ut.
+						Vestibulum nec vestibulum orci, aliquet ullamcorper nisi.</p>
+				</div> <!-- /.col-md-6 -->
+			</div> <!-- /.col-md-6 -->
+
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Column 3</h2>
+					<p>Pellentesque hendrerit eu odio in consectetur. Nullam ante leo, congue eget sapien vel, venenatis
+						dignissim leo. Mauris nisi est, maximus ac convallis ut, ornare quis lorem. Phasellus aliquam, eros
+						vitae porttitor accumsan, augue libero finibus diam, quis tempor libero purus vel orci. Suspendisse
+						eget tortor dapibus, tincidunt lorem eu, mollis odio. Fusce at nisi erat. Proin congue convallis mi,
+						quis faucibus risus vestibulum sed. Proin id tempor nibh. Praesent dapibus efficitur mi. Donec
+						volutpat mauris quis lectus mattis, laoreet congue lacus vestibulum. Donec sit amet metus semper,
+						ultrices arcu ac, dapibus lorem. Aliquam efficitur vulputate justo et varius. Nullam luctus varius
+						nisl. Proin efficitur diam eu posuere facilisis. Vestibulum ut turpis eget ex semper consequat in non
+						lacus. Aliquam nec ipsum mauris. Phasellus ultricies ante arcu, ut mollis erat bibendum eget. Sed id
+						sodales urna, convallis sagittis libero. Vestibulum ornare purus eget nulla porttitor fringilla. Morbi
+						scelerisque at nunc vitae ultrices. Maecenas feugiat velit arcu, sit amet congue mi tempor nec.
+						Vivamus leo dui, aliquam ut molestie nec, semper id dolor. In tincidunt, orci at laoreet pharetra,
+						metus quam ultrices nunc, eget tempor metus augue vel tortor. Vivamus orci odio, fermentum in quam
+						vitae, venenatis blandit urna. Vestibulum gravida dignissim quam, vel cursus ligula maximus ut.
+						Vestibulum nec vestibulum orci, aliquet ullamcorper nisi.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<h2>Column 4</h2>
+					<p>Pellentesque hendrerit eu odio in consectetur. Nullam ante leo, congue eget sapien vel, venenatis
+						dignissim leo. Mauris nisi est, maximus ac convallis ut, ornare quis lorem. Phasellus aliquam, eros
+						vitae porttitor accumsan, augue libero finibus diam, quis tempor libero purus vel orci. Suspendisse
+						eget tortor dapibus, tincidunt lorem eu, mollis odio. Fusce at nisi erat. Proin congue convallis mi,
+						quis faucibus risus vestibulum sed. Proin id tempor nibh. Praesent dapibus efficitur mi. Donec
+						volutpat mauris quis lectus mattis, laoreet congue lacus vestibulum. Donec sit amet metus semper,
+						ultrices arcu ac, dapibus lorem. Aliquam efficitur vulputate justo et varius. Nullam luctus varius
+						nisl. Proin efficitur diam eu posuere facilisis. Vestibulum ut turpis eget ex semper consequat in non
+						lacus.</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Column 5</h2>
+					<p>Pellentesque hendrerit eu odio in consectetur. Nullam ante leo, congue eget sapien vel, venenatis
+						dignissim leo. Mauris nisi est, maximus ac convallis ut, ornare quis lorem. Phasellus aliquam, eros
+						vitae porttitor accumsan, augue libero finibus diam, quis tempor libero purus vel orci. Suspendisse
+						eget tortor dapibus, tincidunt lorem eu, mollis odio. Fusce at nisi erat. Proin congue convallis
+						mi, quis faucibus risus vestibulum sed. Proin id tempor nibh. Praesent dapibus efficitur mi. Donec
+						volutpat mauris quis lectus mattis, laoreet congue lacus vestibulum. Donec sit amet metus semper,
+						ultrices arcu ac, dapibus lorem. Aliquam efficitur vulputate justo et varius. Nullam luctus varius
+						nisl. Proin efficitur diam eu posuere facilisis. Vestibulum ut turpis eget ex semper consequat in
+						non lacus.</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Column 6</h2>
+					<p>Pellentesque hendrerit eu odio in consectetur. Nullam ante leo, congue eget sapien vel, venenatis
+						dignissim leo. Mauris nisi est, maximus ac convallis ut, ornare quis lorem. Phasellus aliquam, eros
+						vitae porttitor accumsan, augue libero finibus diam, quis tempor libero purus vel orci. Suspendisse
+						eget tortor dapibus, tincidunt lorem eu, mollis odio. Fusce at nisi erat. Proin congue convallis
+						mi, quis faucibus risus vestibulum sed. Proin id tempor nibh. Praesent dapibus efficitur mi. Donec
+						volutpat mauris quis lectus mattis, laoreet congue lacus vestibulum. Donec sit amet metus semper,
+						ultrices arcu ac, dapibus lorem. Aliquam efficitur vulputate justo et varius. Nullam luctus varius
+						nisl. Proin efficitur diam eu posuere facilisis. Vestibulum ut turpis eget ex semper consequat in
+						non lacus.</p>
+				</div>
+			</div>
+		</div> <!-- /.container -->
+
+
+		<!-- <h1>This is my web page</h1>
 		<table>
 			<tr>
 				<th>Cats</th>
@@ -67,10 +167,7 @@
 		</ol>
 
 		<h2>I Love Soccer</h2>
-		<img src="images/soccer.jpg" alt="soccer ball"/>
-
-
-
+		<img src="images/soccer.jpg" alt="soccer ball"/> -->
 
 
 	</body>
